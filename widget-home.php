bdcbbaa4529640378469fa3ef703aa96894c993e
Carlos Zambrano <dzambrano@eluniverso.com>
2016-02-16 18:04:40 -0500

    Widgets home
    Widget de Limpieza, consejos y facebook

--- widget-home.php
+++ widget-home.php
@@ -5,6 +5,20 @@
 				<article class="col3">
 					<div class="text">
 						<h2>Limpieza recurrente</h2>
+<h3>¿Qué es una limpieza recurrente?</h3>
+<p>El servicio de limpieza recurrente te permite planificar la limpieza de tu hogar u oficina de manera continua y repetitiva, de acuerdo a la frecuencia con la que tu la requieras.</p>	
+
+<h3>
+Beneficios de la limpieza recurrente</h3>	
+	<ul>
+		<li>Puedes reservar a tu Asistente de Hogar favorita.</li>				
+		<li>Obtienes descuentos especiales de acuerdo a la frecuencia <requerida.</li>	
+		<li>Planificas la limpieza para tus días preferidos.</li>			
+	</ul>
+
+<h3>¿Más información?</h3>
+<p>Para mayor información sobre este servicio llámanos a los teléfonos  <strong>0996179227 - 0996171061</strong> o escríbenos a <a href="mailto:info@easyhogar.com">info@easyhogar.com</a>. Consulta los <a href="#">Términos y condiciones</a> de nuestros servicios.</p>
+
 					</div><!-- /.text -->
 				</article><!-- /.servicio-resumen -->
 				
@@ -12,6 +26,22 @@
 				<article class="col3">
 					<div class="text">
 						<h2>Consejos de limpieza</h2>
+			<ul class="consejos">
+				<li>
+					<h3>Barrer fácilmente</h3>
+					<p>Para lograr un barrido más eficaz que deje cualquier piso libre de cualquier pelusa o mota de polvo, la solución está en envolver la escoba en una media de nylon, con lo cual te asegurarás de recoger hasta lo que no se ve, ya que el nylon tiene la propiedad de cargarse estáticamente y podrás recoger cada pequeña partícula que esté en el suelo.
+					</p>
+				</li>
+
+				<li>
+					<h3>Azulejos impecables</h3>
+					<p>Disuelve un poco de amoniaco en agua y limpia los azulejos con papel periódico humedecido con la mezcla. En vez de usar amoniaco, también puedes usar vinagre. Si están muy sucios, prueba con hacer una pasta con bicarbonato y lejía y a continuación frota con un cepillo y enjuaga bien.
+					</p>
+				</li>		
+			</ul>
+			<center><a href="#" class="btn">Ver m&aacute;s</a></center>
+
+
 					</div><!-- /.text -->
 				</article><!-- /.servicio-resumen -->
 
