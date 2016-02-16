022888c088546392695177dcf4960019a124e1e1
Carlos Zambrano <dzambrano@eluniverso.com>
2016-02-16 18:12:30 -0500

    Correccion de tildes en home

--- widget-home.php
+++ widget-home.php
@@ -5,7 +5,7 @@
 				<article class="col3">
 					<div class="text">
 						<h2>Limpieza recurrente</h2>
-<h3>¿Qué es una limpieza recurrente?</h3>
+<h3>¿Qu&eacute; es una limpieza recurrente?</h3>
 <p>El servicio de limpieza recurrente te permite planificar la limpieza de tu hogar u oficina de manera continua y repetitiva, de acuerdo a la frecuencia con la que tu la requieras.</p>	
 
 <h3>
@@ -16,8 +16,8 @@ Beneficios de la limpieza recurrente</h3>
 		<li>Planificas la limpieza para tus días preferidos.</li>			
 	</ul>
 
-<h3>¿Más información?</h3>
-<p>Para mayor información sobre este servicio llámanos a los teléfonos  <strong>0996179227 - 0996171061</strong> o escríbenos a <a href="mailto:info@easyhogar.com">info@easyhogar.com</a>. Consulta los <a href="#">Términos y condiciones</a> de nuestros servicios.</p>
+<h3>&iquest;M&aacute;s informaci&oacute;n?</h3>
+<p>Para mayor informaci&oacute;n sobre este servicio ll&aacute;manos a los tel&eacute;fonos  <strong>0996179227 - 0996171061</strong> o escríbenos a <a href="mailto:info@easyhogar.com">info@easyhogar.com</a>. Consulta los <a href="#">T&eacute;rminos y condiciones</a> de nuestros servicios.</p>
 
 					</div><!-- /.text -->
 				</article><!-- /.servicio-resumen -->
@@ -28,14 +28,14 @@ Beneficios de la limpieza recurrente</h3>
 						<h2>Consejos de limpieza</h2>
 			<ul class="consejos">
 				<li>
-					<h3>Barrer fácilmente</h3>
-					<p>Para lograr un barrido más eficaz que deje cualquier piso libre de cualquier pelusa o mota de polvo, la solución está en envolver la escoba en una media de nylon, con lo cual te asegurarás de recoger hasta lo que no se ve, ya que el nylon tiene la propiedad de cargarse estáticamente y podrás recoger cada pequeña partícula que esté en el suelo.
+					<h3>Barrer f&aacute;cilmente</h3>
+					<p>Para lograr un barrido m&aacute;s eficaz que deje cualquier piso libre de cualquier pelusa o mota de polvo, la soluci&oacute;n est&aacute; en envolver la escoba en una media de nylon, con lo cual te asegurar&aacute;s de recoger hasta lo que no se ve, ya que el nylon tiene la propiedad de cargarse est&aacute;ticamente y podr&aacute;s recoger cada pequeña partícula que est&eacute; en el suelo.
 					</p>
 				</li>
 
 				<li>
 					<h3>Azulejos impecables</h3>
-					<p>Disuelve un poco de amoniaco en agua y limpia los azulejos con papel periódico humedecido con la mezcla. En vez de usar amoniaco, también puedes usar vinagre. Si están muy sucios, prueba con hacer una pasta con bicarbonato y lejía y a continuación frota con un cepillo y enjuaga bien.
+					<p>Disuelve un poco de amoniaco en agua y limpia los azulejos con papel peri&oacute;dico humedecido con la mezcla. En vez de usar amoniaco, tambi&eacute;n puedes usar vinagre. Si est&aacute;n muy sucios, prueba con hacer una pasta con bicarbonato y lejía y a continuaci&oacute;n frota con un cepillo y enjuaga bien.
 					</p>
 				</li>		
 			</ul>
