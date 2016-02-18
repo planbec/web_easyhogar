e7cbc937b1fcb8291fc1976dd758bc87c968d29d
Carlos Zambrano <dzambrano@eluniverso.com>
2016-02-18 17:57:10 -0500

    Facebook widget

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php include ("code-head.php"); ?>
 
 <body>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
 <?php include("header.php"); ?>
 	
 	<main id="content-area">
--- widget-home.php
+++ widget-home.php
@@ -47,8 +47,12 @@ Beneficios de la limpieza recurrente</h3>
 
 
 				<article class="col3">				
-					<div class="text">
+					<div class="text" style="width: 100%;">
 						<h2>Facebook</h2>
+
+<div class="fb-page" data-href="https://www.facebook.com/multimediaPlanB/" data-width="100%" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/multimediaPlanB/"><a href="https://www.facebook.com/multimediaPlanB/">Agencia multimedia PLAN B</a></blockquote></div></div>
+<!-- div class="fb-page" data-href="https://www.facebook.com/multimediaPlanB/" data-width="100%" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/multimediaPlanB/"><a href="https://www.facebook.com/multimediaPlanB/">Agencia multimedia PLAN B</a></blockquote></div></div -->
+
 					</div><!-- /.text -->
 				</article><!-- /.servicio-resumen -->
 
