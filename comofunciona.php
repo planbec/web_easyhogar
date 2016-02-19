d8509ba707972d07e91c8fadf13d8fd8176baf4f
Carlos Zambrano <dzambrano@eluniverso.com>
2016-02-19 16:02:59 -0500

    Pagina como funciona

---
+++ comofunciona.php
@@ -0,0 +1,83 @@
+<?php include ("code-head.php"); ?>
+
+<body>
+<?php include("header.php"); ?>
+	
+	<main id="content-area">
+	
+		<article class="single-page">
+			
+			
+			<header class="page-header">
+				<div class="container">
+					
+					<h1><i class="fa fa-cogs"></i> &iquest;C&oacute;mo funciona?</h1>
+					
+					<!--p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut massa non ipsum<br />
+					fermentum rhoncus non vel neque. Phasellus varius feugiat purus vel sodales. </p-->
+				
+				</div>
+			</header><!-- /.page-header -->
+			
+			
+			
+			<div class="container page-content">
+			
+				<section class="detalle-servicio">
+					<div class="icono">
+						<figure class="icono-servicio">
+							<i class="fa fa-whatsapp"></i>
+						</figure><!-- /.icono-servicio -->
+					</div><!-- /.icono -->
+					<div class="text">
+						
+						<h3>Nos contactas y agendas tu servicio</h3>
+						
+						<p>Ll&aacute;manos al los tel&eacute;fonos o por email a <a href="mailto:info@easyhogar.com">info@easyhogar.com</a></p>
+					</div><!-- /.text -->
+				</section><!-- /.detalle-servicio -->
+
+
+
+
+				<section class="detalle-servicio">
+					<div class="icono">
+						<figure class="icono-servicio">
+							<i class="fa fa-map-marker"></i>
+						</figure><!-- /.icono-servicio -->
+					</div><!-- /.icono -->
+					<div class="text">
+						
+						<h3>Te visitamos y nos encargamos de la limpieza</h3>
+						
+						<p>Nuestras asistentes de hogar atender&aacute;n tus requerimientos de limpieza</p>
+					</div><!-- /.text -->
+				</section><!-- /.detalle-servicio -->
+
+
+				<section class="detalle-servicio">
+					<div class="icono">
+						<figure class="icono-servicio">
+							<i class="fa fa-home"></i>
+						</figure><!-- /.icono-servicio -->
+					</div><!-- /.icono -->
+					<div class="text">
+						
+						<h3>&iexcl;Eso es todo! Disfruta de tu hogar</h3>
+						
+						<p>Disfruta de tu casa limpia y organizada... como te gusta</p>
+					</div><!-- /.text -->
+				</section><!-- /.detalle-servicio -->
+			
+				
+							
+			</div><!-- /.content -->
+			
+		</article><!-- /#single-page -->
+	</main><!-- /#content-area -->	
+	
+	
+<?php include("footer.php"); ?>
+	
+</body>
+</html>
--- quehacemos.php
+++ quehacemos.php
@@ -24,16 +24,14 @@
 			<div class="container page-content">
 			
 				<section class="detalle-servicio">
-					<div class="icono">
-						<figure class="icono-servicio">
-							<i class="fa fa-whatsapp"></i>
-						</figure><!-- /.icono-servicio -->
-					</div><!-- /.icono -->
+					<div class="foto">
+						<img src="img/limpieza01.jpg">
+					</div><!-- /.foto -->
 					<div class="text">
 						
-						<h3>Nos contactas y agendas tu servicio</h3>
+						<h3>Proveemos de personal de confianza</h3>
 						
-						<p>Ll&aacute;manos al los tel&eacute;fonos o por email a <a href="mailto:info@easyhogar.com">info@easyhogar.com</a></p>
+						<p>para el servicio dom&eacute;stico por horas para la limpieza de hogar y oficinas.</a></p>
 					</div><!-- /.text -->
 				</section><!-- /.detalle-servicio -->
 
@@ -41,31 +39,14 @@
 
 
 				<section class="detalle-servicio">
-					<div class="icono">
-						<figure class="icono-servicio">
-							<i class="fa fa-map-marker"></i>
-						</figure><!-- /.icono-servicio -->
-					</div><!-- /.icono -->
+					<div class="foto">
+						<img src="img/limpieza02.jpg">
+					</div><!-- /.foto -->
 					<div class="text">
 						
-						<h3>Te visitamos y nos encargamos de la limpieza</h3>
+						<h3>Servicio dom&eacute;stico de confianza para la limpieza y cuidado de tu hogar</h3>
 						
-						<p>Nuestras asistentes de hogar atender&aacute;n tus requerimientos de limpieza</p>
-					</div><!-- /.text -->
-				</section><!-- /.detalle-servicio -->
-
-
-				<section class="detalle-servicio">
-					<div class="icono">
-						<figure class="icono-servicio">
-							<i class="fa fa-home"></i>
-						</figure><!-- /.icono-servicio -->
-					</div><!-- /.icono -->
-					<div class="text">
-						
-						<h3>&iexcl;Eso es todo! Disfruta de tu hogar</h3>
-						
-						<p>Disfruta de tu casa limpia y organizada... como te gusta</p>
+						<p>No te preocupes de tr&aacute;mites legares y prestaciones sociales<br>&iexcl;Nosotros lo hacemos por t&iacute;!</p>
 					</div><!-- /.text -->
 				</section><!-- /.detalle-servicio -->
 			
