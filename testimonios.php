0e90d506b14ab791a6f2ee2f6c2c00909a0fbae3
Carlos Zambrano <dzambrano@eluniverso.com>
2016-02-22 10:37:59 -0500

    Iconos y links de menu principal

--- testimonios.php
+++ testimonios.php
@@ -11,7 +11,7 @@
 			<header class="page-header">
 				<div class="container">
 					
-					<h1><i class="fa fa-magic"></i> &iquest;Qu&eacute; hacemos?</h1>
+					<h1><i class="fa fa-commenting-o"></i> Testimonios</h1>
 					
 					<!--p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut massa non ipsum<br />
 					fermentum rhoncus non vel neque. Phasellus varius feugiat purus vel sodales. </p-->
@@ -23,32 +23,32 @@
 			
 			<div class="container page-content">
 			
-				<section class="detalle-servicio">
+				<!-- section class="detalle-servicio">
 					<div class="foto">
 						<img src="img/limpieza01.jpg">
-					</div><!-- /.foto -->
+					</div>
 					<div class="text">
 						
 						<h3>Proveemos de personal de confianza</h3>
 						
 						<p>para el servicio dom&eacute;stico por horas para la limpieza de hogar y oficinas.</a></p>
-					</div><!-- /.text -->
-				</section><!-- /.detalle-servicio -->
+					</div>
+				</section>< -->
 
 
 
 
-				<section class="detalle-servicio">
+				<!-- section class="detalle-servicio">
 					<div class="foto">
-						<img src="img/limpieza02.jpg">
-					</div><!-- /.foto -->
+						<img src="img/limpieza01.jpg">
+					</div>
 					<div class="text">
 						
-						<h3>Servicio dom&eacute;stico de confianza para la limpieza y cuidado de tu hogar</h3>
+						<h3>Proveemos de personal de confianza</h3>
 						
-						<p>No te preocupes de tr&aacute;mites legares y prestaciones sociales<br>&iexcl;Nosotros lo hacemos por t&iacute;!</p>
-					</div><!-- /.text -->
-				</section><!-- /.detalle-servicio -->
+						<p>para el servicio dom&eacute;stico por horas para la limpieza de hogar y oficinas.</a></p>
+					</div>
+				</section>< -->
 			
 				
 							
